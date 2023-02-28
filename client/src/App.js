f8c8f88e4e903846ddb14b00ebe3cc8ae35603ff
lestercoyoy <52750976+lestercoyoyjr@users.noreply.github.com>
2023-02-28 00:01:25 -0600

    ADD
    First post and some CSS for it

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,31 @@ import './App.css';
 
 function App() {
   return (
-    <div>test</div>
+    <main>
+      <header>
+        <a href="" className="logo">MyBlog</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+
+        </nav>
+      </header>
+      <div className="post">
+        <img src="https://techcrunch.com/wp-content/uploads/2022/09/hand_raise.png?w=1390&crop=1" alt=""></img>
+        <h2>Connect with Mayfield, JETRO, Toptal and more at TechCrunch Disrupt</h2>
+        <p>Mark your calendars, startup fans, because TechCrunch is returning — live and in person — to the beautiful City by the Bay to host our flagship event, TechCrunch Disrupt, on October 18–20 at the Moscone West Convention Center followed by an online recap event on October 21.</p>
+      </div>
+      <div className="post">
+        <img src="https://techcrunch.com/wp-content/uploads/2022/09/hand_raise.png?w=1390&crop=1" alt=""></img>
+        <h2>Connect with Mayfield, JETRO, Toptal and more at TechCrunch Disrupt</h2>
+        <p>Mark your calendars, startup fans, because TechCrunch is returning — live and in person — to the beautiful City by the Bay to host our flagship event, TechCrunch Disrupt, on October 18–20 at the Moscone West Convention Center followed by an online recap event on October 21.</p>
+      </div>
+      <div className="post">
+        <img src="https://techcrunch.com/wp-content/uploads/2022/09/hand_raise.png?w=1390&crop=1" alt=""></img>
+        <h2>Connect with Mayfield, JETRO, Toptal and more at TechCrunch Disrupt</h2>
+        <p>Mark your calendars, startup fans, because TechCrunch is returning — live and in person — to the beautiful City by the Bay to host our flagship event, TechCrunch Disrupt, on October 18–20 at the Moscone West Convention Center followed by an online recap event on October 21.</p>
+      </div>
+    </main>
   );
 }
 
