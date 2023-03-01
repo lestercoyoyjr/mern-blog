7d14ed744ed587b2084193c49db9e71af615b528
lestercoyoy <52750976+lestercoyoyjr@users.noreply.github.com>
2023-02-28 23:55:53 -0600

    MODIFY
    - Add img max width
    - Add div.post  and div.texts classes

--- client/src/App.js
+++ client/src/App.js
@@ -12,20 +12,33 @@ function App() {
         </nav>
       </header>
       <div className="post">
+        <div className="Image">
         <img src="https://techcrunch.com/wp-content/uploads/2022/09/hand_raise.png?w=1390&crop=1" alt=""></img>
+        </div>
+        <div className="texts">
         <h2>Connect with Mayfield, JETRO, Toptal and more at TechCrunch Disrupt</h2>
-        <p>Mark your calendars, startup fans, because TechCrunch is returning — live and in person — to the beautiful City by the Bay to host our flagship event, TechCrunch Disrupt, on October 18–20 at the Moscone West Convention Center followed by an online recap event on October 21.</p>
+          <p>Mark your calendars, startup fans, because TechCrunch is returning — live and in person — to the beautiful City by the Bay to host our flagship event, TechCrunch Disrupt, on October 18–20 at the Moscone West Convention Center followed by an online recap event on October 21.</p>
+        </div>
       </div>
       <div className="post">
+        <div className="Image">
         <img src="https://techcrunch.com/wp-content/uploads/2022/09/hand_raise.png?w=1390&crop=1" alt=""></img>
+        </div>
+        <div className="texts">
         <h2>Connect with Mayfield, JETRO, Toptal and more at TechCrunch Disrupt</h2>
-        <p>Mark your calendars, startup fans, because TechCrunch is returning — live and in person — to the beautiful City by the Bay to host our flagship event, TechCrunch Disrupt, on October 18–20 at the Moscone West Convention Center followed by an online recap event on October 21.</p>
+          <p>Mark your calendars, startup fans, because TechCrunch is returning — live and in person — to the beautiful City by the Bay to host our flagship event, TechCrunch Disrupt, on October 18–20 at the Moscone West Convention Center followed by an online recap event on October 21.</p>
+        </div>
       </div>
       <div className="post">
+        <div className="Image">
         <img src="https://techcrunch.com/wp-content/uploads/2022/09/hand_raise.png?w=1390&crop=1" alt=""></img>
+        </div>
+        <div className="texts">
         <h2>Connect with Mayfield, JETRO, Toptal and more at TechCrunch Disrupt</h2>
-        <p>Mark your calendars, startup fans, because TechCrunch is returning — live and in person — to the beautiful City by the Bay to host our flagship event, TechCrunch Disrupt, on October 18–20 at the Moscone West Convention Center followed by an online recap event on October 21.</p>
+          <p>Mark your calendars, startup fans, because TechCrunch is returning — live and in person — to the beautiful City by the Bay to host our flagship event, TechCrunch Disrupt, on October 18–20 at the Moscone West Convention Center followed by an online recap event on October 21.</p>
+        </div>
       </div>
+      
     </main>
   );
 }
