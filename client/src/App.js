5d65e0ea3d4b4885020e96c31b8cb230fbaff148
lestercoyoy <52750976+lestercoyoyjr@users.noreply.github.com>
2023-03-01 22:33:04 -0600

    MODIFY
    It's required to make login page works first and then proceed to add css layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,26 @@
-import './App.css';
-import Header from './Header';
-import Post from './Post';
+import "./App.css";
+import Header from "./Header";
+import Post from "./Post";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <main>
-      <Header/>
-      <Post/>
-      <Post/>
-      <Post/>
-      
-    </main>
+    <Routes>
+      <Route index element={
+        <main>
+          <Header />
+          <Post />
+          <Post />
+          <Post />
+        </main>
+      } />
+      <Route path={'/login'} element={
+        <main>
+          <Header/>
+          <div>login page</div>
+        </main>
+      } />
+    </Routes>
   );
 }
 
