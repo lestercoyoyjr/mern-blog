875dc5162a79420f30989f2e424f751841043c51
lestercoyoy <52750976+lestercoyoyjr@users.noreply.github.com>
2023-03-01 22:06:04 -0600

    MODIFY
    We call the header function

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,11 @@
 import './App.css';
+import Header from './Header';
 import Post from './Post';
 
 function App() {
   return (
     <main>
-      <header>
-        <a href="" className="logo">MyBlog</a>
-        <nav>
-          <a href="">Login</a>
-          <a href="">Register</a>
-
-        </nav>
-      </header>
+      <Header/>
       <Post/>
       <Post/>
       <Post/>
