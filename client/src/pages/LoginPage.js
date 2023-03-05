87b0cf36f0f25053a71e98850ce83227dc96cd95
lestercoyoy <52750976+lestercoyoyjr@users.noreply.github.com>
2023-03-04 23:59:16 -0600

    MODIFY
    Login basic form

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,5 +1,9 @@
 export default function LoginPage() {
     return(
-        <div> Login Page </div>
+        <form action="">
+            <input type="text" placeholder="username"></input>
+            <input type="password" placeholder="password"></input>
+            <button>Login</button>
+        </form>
     );
 }
