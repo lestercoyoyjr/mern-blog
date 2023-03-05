e488f002bbf474ddc157c4a295c5b01ca26cda79
lestercoyoy <52750976+lestercoyoyjr@users.noreply.github.com>
2023-03-05 00:05:34 -0600

    ADD
    Register basic settings

---
+++ client/src/pages/RegisterPage.js
@@ -0,0 +1,9 @@
+export default function RegisterPage(){
+    return(
+        <form action="">
+            <input type="text" placeholder="username"></input>
+            <input type="password" placeholder="password"></input>
+            <button>Register</button>
+        </form>
+    )
+}
