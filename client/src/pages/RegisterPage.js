4b163ae5eec7b8be8818439904d194e492c00ac7
lestercoyoy <52750976+lestercoyoyjr@users.noreply.github.com>
2023-03-06 23:52:39 -0600

    ADD
    Register form base (not finished yet)

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,9 +1,22 @@
+import {userState} from "react";
+
 export default function RegisterPage() {
+    const [username, setUsername] = userState('');
+    const [password, setPassword] = userState('');
+    async function register(ev){
+        ev.preventDefault();
+        await fetch('http://localhost:4000/register', {
+            method: 'POST',
+            body: JSON.stringify({username, password}),
+            headers: {'Content-type':'application/json'}
+        })
+    }
+
     return(
-        <form className="register">
+        <form className="register" onSubmit={register}>
             <h1>Register</h1>
-            <input type="text" placeholder="username"></input>
-            <input type="password" placeholder="password"></input>
+            <input type="text" placeholder="username" value={username} onChange={ ev => setUsername(ev.target.value)}></input>
+            <input type="password" placeholder="password" value={password} onChange={ev => setPassword(ev.target.value)}></input>
             <button>Register</button>
         </form>
     );
