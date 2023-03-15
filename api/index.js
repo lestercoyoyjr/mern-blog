4690e592ec632a079d20345f591d4a08a95a7b75
lestercoyoy <52750976+lestercoyoyjr@users.noreply.github.com>
2023-03-14 23:32:55 -0600

    ADD
    MongoDB Connection using .env file

--- api/index.js
+++ api/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { default: mongoose } = require('mongoose');
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI);
 
 app.post('/register', (req,res) => {
     const {username,password} = req.body;
