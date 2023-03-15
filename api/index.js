eb4a5799b281c6448daa3709b8c89c82e543f64c
lestercoyoy <52750976+lestercoyoyjr@users.noreply.github.com>
2023-03-15 00:28:26 -0600

    ADD
    Create user in MongoDB

--- api/index.js
+++ api/index.js
@@ -1,17 +1,20 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
 const { default: mongoose } = require('mongoose');
 require("dotenv").config();
+const User = require('./models/User');
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI);
 
-app.post('/register', (req,res) => {
+
+app.post('/register', async (req,res) => {
     const {username,password} = req.body;
-    res.json({requestData:{username,password}});
+    const userDoc = await User.create({username, password});
+    res.json(userDoc);
 })
 
 app.listen(4000)
