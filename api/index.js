1d42c1e1ded97bc969fbdf2e5ba5d966fbdb2248
lestercoyoy <52750976+lestercoyoyjr@users.noreply.github.com>
2023-03-22 01:01:02 -0600

    ADD
    - JWT Token

--- api/index.js
+++ api/index.js
@@ -5,9 +5,11 @@ require("dotenv").config();
 const User = require('./models/User');
 const bcrypt = require('bcryptjs');
 const app = express();
+const jwt = require('jsonwebtoken');
 
 // in order to encrypt password
 const salt = bcrypt.genSaltSync(10);
+const secret = 'sdfio43rm0934lkds2340fdsga';
 
 app.use(cors());
 app.use(express.json());
@@ -31,7 +33,16 @@ app.post('/login', async (req,res) => {
     const{username, password} = req.body;
     const userDoc = await User.findOne({username});
     const passOk = bcrypt.compareSync(password, userDoc.password);
-    res.json(passOk);
+    if (passOk) {
+        // logged in
+        jwt.sign({username, id:userDoc._id}, secret, {}, (err,token) => {
+            if (err) throw err;
+            res.json(token);
+        });
+        // res.json();
+    } else {
+        res.status(400).json('wrong credentials')
+    }
 });
 
 app.listen(4000)
